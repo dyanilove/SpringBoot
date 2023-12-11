4afaf5163b6b772efe5a55e06026fe003d3fbc00
dyanilove <ekdms216@nate.com>
2023-12-12 00:20:29 +0900

    second commit

--- ch1/src/main/java/com/example/ch1/Ch1Application.java
+++ ch1/src/main/java/com/example/ch1/Ch1Application.java
@@ -2,7 +2,10 @@ package com.example.ch1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Ch1Application {
 
@@ -10,4 +13,8 @@ public class Ch1Application {
 		SpringApplication.run(Ch1Application.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(){
+		return "Hello Spring Boot";
+	}
 }
---
+++ ch2/src/main/java/com/example/ch2/Main.java
@@ -0,0 +1,7 @@
+package com.example.ch2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
