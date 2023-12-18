adb517c1f3e78801d9d36a10eef48fe096c09709
dyanilove <ekdms216@nate.com>
2023-12-19 01:06:02 +0900

    sixth commit

--- ch2/src/main/java/com/example/ch2/LoginController.java
+++ ch2/src/main/java/com/example/ch2/LoginController.java
@@ -1,11 +1,13 @@
 package com.example.ch2;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.net.URLEncoder;
 
@@ -20,7 +22,7 @@ public class LoginController {
 
 //    @RequestMapping(value = "/login", method = RequestMethod.POST)
     @PostMapping("/login")
-    public String login(String id, String pwd, Model model) throws Exception{
+    public String login(HttpServletRequest req, String id, String pwd, RedirectAttributes model) throws Exception{
         // 1. id,pwd를 확인
         if(loginCheck(id,pwd)){
             model.addAttribute("id",id);
@@ -28,9 +30,14 @@ public class LoginController {
             // 2. 일치하면, userInfo.html
             return "userInfo";
         }else {
-            String msg = URLEncoder.encode("id또는 pwd가 일치하지 않습니다.","utf-8") ;
+//            String msg = URLEncoder.encode("id또는 pwd가 일치하지 않습니다.","utf-8") ;
+            String msg = "id또는 pwd가 일치하지 않습니다." ;
 //            일치하지 않으면, login.html
-            return "redirect:/login/login?msg="+msg;
+//            return "redirect:/login/login?msg="+msg;
+            model.addAttribute("msg", msg);
+            model.addFlashAttribute("msg", "일회용 메시지");
+            req.setAttribute("msg","request에 저장된 msg");
+            return "redirect:/";
         }
     }
 
