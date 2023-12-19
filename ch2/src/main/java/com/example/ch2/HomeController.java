fb4b5d00b354e66245bb88e4442cb0b9f9468624
dyanilove <ekdms216@nate.com>
2023-12-20 00:20:11 +0900

    seventh commit

--- ch2/src/main/java/com/example/ch2/HomeController.java
+++ ch2/src/main/java/com/example/ch2/HomeController.java
@@ -1,7 +1,11 @@
 package com.example.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 //1. 원격 프로그램으로 등록
 @Controller
@@ -10,5 +14,13 @@ public class HomeController {  // 원격 프로그램
     @RequestMapping("/")
     public String main() {
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("lastName","daeun");
+        model.addAttribute("firstName","Hwang");
+        model.addAttribute("list", Arrays.asList("aaa","bbb","ccc","ddd","eee"));
+        return "test";
     }
 }
