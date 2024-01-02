0f3f7d1e4764e75f7cbae44ed5b8a03edbe0548a
dutlsghkd216 <dutlsghkd@naver.com>
2024-01-02 17:25:12 +0900

    day 13

---
+++ ch3/src/main/java/com/example/ch3/di2/Main.java
@@ -0,0 +1,50 @@
+package com.example.ch3.di2;
+
+class Car{
+    Engine engine;
+    Door door;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine=" + engine +
+                ", door=" + door +
+                '}';
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setDoor(Door door) {
+        this.door = door;
+    }
+}
+class SportsCar extends Car{
+    @Override
+    public String toString() {
+        return "SportsCar{" +
+                "engine=" + engine +
+                ", door=" + door +
+                '}';
+    }
+}
+class Engine{
+
+}
+class Door{
+
+}
+public class Main {
+    public static void main(String[] args) {
+        AppContext ac = new AppContext(AppConfig.class);
+        Car car = (Car)ac.getBean("car");    //byName
+        Car car2 = (Car)ac.getBean(Car.class);    //byType
+        Engine engine = (Engine)ac.getBean("engine");
+        Door door = (Door)ac.getBean(Door.class);
+        System.out.println("car = " + car);
+        System.out.println("car2 = " + car2);
+        System.out.println("engine = " + engine);
+        System.out.println("door = " + door);
+    }
+}
