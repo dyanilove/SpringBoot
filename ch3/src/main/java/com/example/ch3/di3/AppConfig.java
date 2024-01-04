bad80a32bafcd5c521fb51ae8c65dd3d2d82d787
dutlsghkd216 <dutlsghkd@naver.com>
2024-01-04 22:13:47 +0900

    day15

--- ch3/src/main/java/com/example/ch3/di3/AppConfig.java
+++ ch3/src/main/java/com/example/ch3/di3/AppConfig.java
@@ -1,22 +1,24 @@
 package com.example.ch3.di3;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration      // 일반클래스가아니라 설정클래스라는걸 의미
+@ComponentScan
 public class AppConfig {
-    @Bean
-    Car car(){
-        return new Car();
-    }
-
-    @Bean
-    Engine engine(){
-        return new Engine();
-    }
-
-    @Bean
-    Door door(){
-        return new Door();
-    }
+//    @Bean
+//    Car car(){
+//        return new Car();
+//    }
+//
+//    @Bean
+//    Engine engine(){
+//        return new Engine();
+//    }
+//
+//    @Bean
+//    Door door(){
+//        return new Door();
+//    }
 }
