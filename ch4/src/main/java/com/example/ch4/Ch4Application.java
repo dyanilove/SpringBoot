dd4ce17835471683161d9fc037d6e79c42de5bbd
dutlsghkd216 <dutlsghkd@naver.com>
2024-01-08 20:08:19 +0900

    day 17

---
+++ ch4/src/main/java/com/example/ch4/Ch4Application.java
@@ -0,0 +1,25 @@
+package com.example.ch4;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4Application implements CommandLineRunner {
+	@Autowired
+	EntityManagerFactory emf;
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Ch4Application.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("emf = " + emf);
+	}
+}
